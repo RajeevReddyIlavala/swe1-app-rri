bf4f1743a60cf6ffd063c0593836ab4ceae9f843
rajeev <rajeev.ilavala@gmail.com>
2019-10-18 20:20:03 -0400

    fixed issues raised by flake8

--- polls/models.py
+++ polls/models.py
@@ -13,11 +13,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-    	return self.question_text
+        return self.question_text
 
     def was_published_recently(self):
-    	now = timezone.now()
-    	return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -29,5 +29,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-    	return self.choice_text
-
+        return self.choice_text
